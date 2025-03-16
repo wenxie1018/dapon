bcd3f78d66a3198b7873c6d87bcff71beeba2464
wenxie1018 <wenxie1018@gmail.com>
2025-03-16 19:50:37 +0800

    fifth commit

--- final/settings.py
+++ final/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'final.urls'
@@ -123,6 +124,7 @@ STATIC_URL='/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'home', 'static')]
 #STATICFILES_DIRS=(os.path.join(HOME_DIR,'static/'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
